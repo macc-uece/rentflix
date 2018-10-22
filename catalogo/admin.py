7bb99fbbb64e18871da3273c91a1cfb38344ab3c
Luis Alves <luisalves05@gmail.com>
2018-10-22 14:37:57 -0300

    Registrando modelos no admin panel

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from catalogo.models import Filme, Genero, FilmeInstancia, Diretor 
 
-# Register your models here.
+
+@admin.register(Filme)
+class FilmeAdmin(admin.ModelAdmin):
+    list_diplay = ('titulo', 'diretor', 'display_genero')
+
+admin.site.register(Genero)
+admin.site.register(Diretor)
