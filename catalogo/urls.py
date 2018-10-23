f01701d0331d2a722d1719a0f3ed9ffac5ecd121
thayanneFranca <thayanne.f.silva@gmail.com>
2018-10-23 17:05:42 -0300

    adicionando template simples da lista de todos os filmes

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -3,4 +3,8 @@ from catalogo import views
 
 urlpatterns = [
    path('', views.index, name='index'), 
+   path('filmes/', views.FilmeListView.as_view(), name='filmes'),
+   path('filme/<int:pk>', views.FilmeDetailView.as_view(), name='filme-info'),
+   path('diretores/', views.DiretorListView.as_view(), name='diretores'),
+   path('diretor/<int:pk>', views.DiretorDetailView.as_view(), name='diretor-info'),
 ]
