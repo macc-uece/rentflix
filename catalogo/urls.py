bc774627c1a6f56babe52a98708df732551d2db5
thayanneFranca <thayanne.f.silva@gmail.com>
2018-10-22 21:49:51 -0300

    adicionando pagina inicial do usuario simples

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from catalogo import views
 
 urlpatterns = [
-   path('', views.index, name='index'),     
+   path('', views.index, name='index'), 
 ]
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
-
-# Create your views here.
+from catalogo.models import Filme, Diretor, FilmeInstancia, Genero
 
 def index(request):
-    pass
+
+    num_filmes = Filme.objects.all().count()
+    num_instancias = FilmeInstancia.objects.all().count()
+    
+
+    num_instancias_disponiveis = FilmeInstancia.objects.filter(status__exact='d').count()
+    
+    # The 'all()' is implied by default.    
+    num_diretores = Diretor.objects.count()
+    
+    context = {
+        'num_filmes': num_filmes,
+        'num_instancias': num_instancias,
+        'num_instancias_disponiveis':  num_instancias_disponiveis,
+        'num_diretores': num_diretores,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
