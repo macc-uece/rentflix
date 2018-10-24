a892e303a828e96a8b1e168d7261da8470b283d4
thayanneFranca <thayanne.f.silva@gmail.com>
2018-10-24 11:53:01 -0300

    Adicionando templantes de lista de diretores e gêneros

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
    path('filme/<int:pk>', views.FilmeDetailView.as_view(), name='filme-info'),
    path('diretores/', views.DiretorListView.as_view(), name='diretores'),
    path('diretor/<int:pk>', views.DiretorDetailView.as_view(), name='diretor-info'),
+   path('generos/', views.GeneroListView.as_view(), name='generos'),
+   path('genero/<int:pk>', views.GeneroDetailView.as_view(), name='genero-info'),
 ]
